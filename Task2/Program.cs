f33d96d4765f33d52cb9944d444cf7b212c59127
Михаил Глушков <stud108055@vyatsu.ru>
2020-06-17 00:18:47 +0300

    1

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -12,7 +12,10 @@ namespace Task2
 		{
 			Polynomial a = new Polynomial(new double[4] { 1, -2, 3, -4 });
 			Polynomial b = new Polynomial(new double[5] { 5, 4, 3, 2, 1 });
+			Polynomial c = new Polynomial(new double[4] { 1, 2, 3, 4 });
+			Polynomial d = new Polynomial(new double[1] { 2 });
 			Console.WriteLine(a + b);
+			Console.WriteLine(c * d);
 		}
 	}
 }
